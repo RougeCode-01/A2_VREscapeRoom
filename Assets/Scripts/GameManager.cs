6b7a8243a6f1a1bdd5d172cf39ff29939bd49589
rougecode <koenigrojass@gmail.com>
2025-03-19 18:54:01 -0600

    Edits to project

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -4,6 +4,7 @@ using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
+    [SerializeField] private GameObject ExitCollider;
     private void Update()
     {
         if (Input.GetKeyDown(KeyCode.R))
@@ -11,9 +12,30 @@ public class GameManager : MonoBehaviour
             RestartLevel();
         }
     }
-
+    
     public void RestartLevel()
     {
         SceneManager.LoadScene("EscapeRoom");
     }
+
+    private void FailedLevel()
+    {
+        //if the timer runs out
+        //Fail the player
+        //Play a sound
+        //Load the game over scene
+        SceneManager.LoadScene("GameOverScene");
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        // Check if the player has collided with the exit collider
+        if (other.CompareTag("Player"))
+        {
+            // Load the next level
+            SceneManager.LoadScene("CompleationScene");
+        }
+    }
+
+    
 }
